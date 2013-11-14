62eceef2d9a7923339d9c69acf1aafe1657e082e
Ismael Ramón Barcones <desko27@gmail.com>
2013-11-14 17:43:25 +0100

    Starting project: initial interface idea.

---
+++ src/MainForm.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace AnimeStorage
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        // ==================================================
+        # region menu bar events
+        // ==================================================
+
+        private void mLog_CheckedChanged(object sender, EventArgs e)
+        {
+            if (mLog.Checked) {
+                mLog.BackColor = Color.DarkBlue;
+                mLog.ForeColor = SystemColors.Window;
+            } else {
+                mLog.BackColor = SystemColors.Control;
+                mLog.ForeColor = SystemColors.ControlText;
+            }
+        }
+
+        private void mRules_Click(object sender, EventArgs e)
+        {
+            var f = new RulesForm();
+            f.StartPosition = FormStartPosition.CenterParent;
+            f.ShowDialog();
+        }
+
+        # endregion
+    }
+}
